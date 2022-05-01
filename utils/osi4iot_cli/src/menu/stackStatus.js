757ad9c23c9caa14297dd42aa057e47fc9c5e51a
osi4iot <osi4iot@gmail.com>
2022-05-01 18:33:12 +0200

    Several correction in osi4iot_cli 4

--- utils/osi4iot_cli/src/menu/stackStatus.js
+++ utils/osi4iot_cli/src/menu/stackStatus.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { execSync } from 'child_process';
 import clc from 'cli-color';
 import { chooseOption } from './chooseOption.js';
+import findManagerDockerHost from './findManagerDockerHost.js';
 
 export default async function () {
 	if (!fs.existsSync('./osi4iot_state.json')) {
@@ -12,22 +13,24 @@ export default async function () {
 		const osi4iotState = JSON.parse(osi4iotStateText);
 		const dockerHost = findManagerDockerHost(osi4iotState.platformInfo.NODES_DATA);
 
-		const response1 = execSync(`docker ${dockerHost} node ls`).toString();
-		const numSwarmNodes = response1.split('\n').length - 2;
-		if (numSwarmNodes === 0) {
-			console.log(clc.redBright("There is no node joined to the swarm cluster. \nUse the command 'osi4iot add_nodes' to get it."));
-		} else {
-			console.log(clc.whiteBright("\nNodes in the swarm cluster:\n"));
-			console.log(response1);
-		}
+		try {
+			const response1 = execSync(`docker ${dockerHost} node ls`).toString();
+			const numSwarmNodes = response1.split('\n').length - 2;
+			if (numSwarmNodes !== 0) {
+				console.log(clc.whiteBright("\nNodes in the swarm cluster:\n"));
+				console.log(response1);
 
-		const response2 = execSync(`docker ${dockerHost} service ls`).toString();
-		const numServices = response2.split('\n').length - 2;
-		if (numServices === 0) {
-			console.log(clc.redBright("No stack has been deployed. \nUse the command 'osi4iot run' to deployed it."));
-		} else {
-			console.log(clc.whiteBright("\nServices running in the platform:\n"));
-			console.log(response2);
+				const response2 = execSync(`docker ${dockerHost} service ls`).toString();
+				const numServices = response2.split('\n').length - 2;
+				if (numServices !== 0) {
+					console.log(clc.whiteBright("\nServices running in the platform:\n"));
+					console.log(response2);
+				} else {
+					console.log(clc.yellowBright("No stack has been deployed. \nUse the command 'osi4iot run' to deployed it."));
+				}
+			}
+		} catch (err2) {
+			console.log(clc.yellowBright("There is no node joined to the swarm cluster. \nUse the command 'osi4iot add_nodes' to get it.\n"));
 		}
 
 		console.log("");
