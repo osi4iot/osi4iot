5f55b14bf9f412c9175794bddcb18e53e4239dfc
osi4iot <osi4iot@gmail.com>
2022-05-01 18:12:07 +0200

    Several correction in osi4iot_cli 2

--- utils/osi4iot_cli/src/menu/stackStatus.js
+++ utils/osi4iot_cli/src/menu/stackStatus.js
@@ -3,13 +3,32 @@ import clc from 'cli-color';
 import { chooseOption } from './chooseOption.js';
 
 export default async function () {
-	const response = execSync("docker service ls").toString();
-	const numServices = response.split('\n').length - 2;
-	if (numServices === 0) {
-		console.log(clc.redBright("No stack has been deployed. \nUse the command 'osi4iot run' to deployed it."));
+	if (!fs.existsSync('./osi4iot_state.json')) {
+		console.log(clc.redBright("The file osi4iot_state.json not exist. \nUse the command 'osi4iot init' to create it."));
+		return;
 	} else {
-		console.log(clc.whiteBright("\nServices running in the platform:\n"));
-		console.log(response);
+		const osi4iotStateText = fs.readFileSync('./osi4iot_state.json', 'UTF-8');
+		const osi4iotState = JSON.parse(osi4iotStateText);
+		const dockerHost = findManagerDockerHost(osi4iotState.platformInfo.NODES_DATA);
+
+		const response1 = execSync(`docker ${dockerHost} node ls`).toString();
+		const numSwarmNodes = response1.split('\n').length - 2;
+		if (numSwarmNodes === 0) {
+			console.log(clc.redBright("There is no node joined to the swarm cluster. \nUse the command 'osi4iot add_nodes' to get it."));
+		} else {
+			console.log(clc.whiteBright("\nNodes in the swarm cluster:\n"));
+			console.log(response1);
+		}
+
+		const response2 = execSync(`docker ${dockerHost} service ls`).toString();
+		const numServices = response2.split('\n').length - 2;
+		if (numServices === 0) {
+			console.log(clc.redBright("No stack has been deployed. \nUse the command 'osi4iot run' to deployed it."));
+		} else {
+			console.log(clc.whiteBright("\nServices running in the platform:\n"));
+			console.log(response2);
+		}
+
 		console.log("");
 		chooseOption();
 	}
