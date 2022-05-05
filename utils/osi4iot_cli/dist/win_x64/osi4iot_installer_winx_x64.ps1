b4c51c8bc41adfca2aa6e6ee7521c23c6bd15750
osi4iot <osi4iot@gmail.com>
2022-05-05 18:35:09 +0200

    Fixing osi4iot_installer_winx_x64.ps1

--- utils/osi4iot_cli/dist/win_x64/osi4iot_installer_winx_x64.ps1
+++ utils/osi4iot_cli/dist/win_x64/osi4iot_installer_winx_x64.ps1
@@ -6,21 +6,32 @@ if ([string]::IsNullOrWhiteSpace($DestinationDrive))
 }
 
 if (Get-PSDrive $DestinationDrive -ErrorAction SilentlyContinue) {
-    $DestinationFolder = "${DestinationDrive}:\osi4iot_cli"
-    if (-not (Test-Path $DestinationFolder)) {
-        New-Item  $DestinationFolder -ItemType Directory | Out-Null
-    }
-    $DestinationPath = "$DestinationFolder\osi4iot.exe"
-    $source = 'https://raw.githubusercontent.com/osi4iot/osi4iot/master/utils/osi4iot_cli/dist/win_x64/osi4iot.exe'
-    $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -Uri $source -OutFile $DestinationPath
-    $ProgressPreference = 'Continue'
+    $UserName = [Environment]::UserName
 
-    $positionfirstspace=$env:Path.IndexOf($DestinationFolder)
-    if( $positionfirstspace -eq -1) {
-        $newPath = "${env:Path}; ${DestinationFolder};"
-        [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
-        $env:Path = $newPath
+    $UserFolder = "${DestinationDrive}:\Users\${UserName}"
+    if ((Test-Path $UserFolder)) {
+        $DestinationFolder = "${UserFolder}\osi4iot_cli"
+        if (-not (Test-Path $DestinationFolder)) {
+            New-Item  $DestinationFolder -ItemType Directory | Out-Null
+        }
+
+        $DestinationPath = "$DestinationFolder\osi4iot.exe"
+        Write-Host "Downloading osi4iot_cli..."
+        $source = 'https://raw.githubusercontent.com/osi4iot/osi4iot/master/utils/osi4iot_cli/dist/win_x64/osi4iot.exe'
+        $ProgressPreference = 'SilentlyContinue'
+        Invoke-WebRequest -Uri $source -OutFile $DestinationPath
+        $ProgressPreference = 'Continue'
+        Write-Host "Osi4iot_cli file downloaded."
+
+        Write-Host "Updating path."
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', 'User') -split ';'
+        if ($persistedPaths -notcontains $DestinationFolder) {
+            $persistedPaths = $persistedPaths + $DestinationFolder | where { $_ }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', 'User')
+        }
+    } else {
+        $Warning = "The folder $UserFolder not exits."
+        Write-Host $Warning
     }
 } else {
     $Warning = "The drive $DestinationDrive not exits."
