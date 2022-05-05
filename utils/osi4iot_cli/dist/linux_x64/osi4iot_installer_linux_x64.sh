d3b3be002967bae5aeaa3368afd9aabec04c2a7d
osi4iot <osi4iot@gmail.com>
2022-05-05 19:01:01 +0200

    Fixing osi4iot_installer problems 3

--- utils/osi4iot_cli/dist/linux_x64/osi4iot_installer_linux_x64.sh
+++ utils/osi4iot_cli/dist/linux_x64/osi4iot_installer_linux_x64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Dowloading osi4iot_cli..."
-curl -o osi4iot https://raw.githubusercontent.com/osi4iot/osi4iot/master/utils/osi4iot_cli/dist/linux_x64/osi4iot
+curl -o osi4iot https://raw.githubusercontent.com/osi4iot/osi4iot/master/utils/osi4iot_cli/dist/linux_x64/osi4iot &> /dev/null
 echo "osi4iot_cli downloaded."
 sudo mv osi4iot /usr/local/bin/osi4iot
 sudo chmod +x /usr/local/bin/osi4iot
