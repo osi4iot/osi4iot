492c11094a722b65c766b9ad281b634f1d7592be
osi4iot <osi4iot@gmail.com>
2022-05-12 07:52:11 +0200

    Script for add osi4iot user in node 2

--- utils/osi4iot_cli/installation_scripts/add_osi4iot_user.sh
+++ utils/osi4iot_cli/installation_scripts/add_osi4iot_user.sh
@@ -6,5 +6,5 @@ if ! id -u "osi4iot" >/dev/null 2>&1; then
     sudo usermod -aG sudo osi4iot
     sudo usermod -aG docker osi4iot
     echo "osi4iot ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/osi4iot
-    chmod 0440 /etc/sudoers.d/osi4iot
+    sudo chmod 0440 /etc/sudoers.d/osi4iot
 fi
