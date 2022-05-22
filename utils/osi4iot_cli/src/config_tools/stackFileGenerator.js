52c3b7f1d2769ab99787b1dd25d21d965a5579b5
osi4iot <osi4iot@gmail.com>
2022-05-22 20:26:40 +0200

    Correction in stackFileGenerator 10

--- utils/osi4iot_cli/src/config_tools/stackFileGenerator.js
+++ utils/osi4iot_cli/src/config_tools/stackFileGenerator.js
@@ -680,9 +680,9 @@ export default function (osi4iotState) {
 			"traefik.enable=true",
 			`traefik.http.routers.mqtt_websocket.rule=Host(\`${domainName}\`)`,
 			"traefik.http.routers.mqtt_websocket.entrypoints=websocket",
-			"traefik.http.routers.mqtt-websockets.tls=true",
+			"traefik.http.routers.mqtt_websockets.tls=true",
 			"traefik.http.routers.mqtt_websocket.tls.certresolver=osi4iot_resolver",
-			"traefik.http.routers.mqtt-websockets.service=mqtt-websockets",
+			"traefik.http.routers.mqtt_websockets.service=mqtt_websockets",
 			"traefik.http.services.mqtt_websocket.loadbalancer.server.port=9001",
 		];
 
