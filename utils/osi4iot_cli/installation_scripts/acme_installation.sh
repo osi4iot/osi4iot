1b42c660e1f83ab80bc1ffe2275ca661e5493d40
osi4iot <osi4iot@gmail.com>
2022-06-06 17:13:32 +0200

    Developing acme.sh option 4

--- utils/osi4iot_cli/installation_scripts/acme_installation.sh
+++ utils/osi4iot_cli/installation_scripts/acme_installation.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 admin_email=$1
-if [ ! -d ./.acme.sh/ ]; then
+if [ ! -d ~/.acme.sh/ ]; then
     git clone https://github.com/acmesh-official/acme.sh.git
-    ./acme.sh/acme.sh --install -m $admin_email
-    ./acme.sh/acme.sh --uninstall-cronjob
+    cd ./acme.sh
+    ./acme.sh --install -m $admin_email
+    ./acme.sh --uninstall-cronjob
+    cd ..
     rm -rf ./acme.sh
 fi
