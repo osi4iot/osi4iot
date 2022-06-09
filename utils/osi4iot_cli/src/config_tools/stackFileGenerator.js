41c3c83cac3cb9cb044d66c73a7bbf049e2007a3
osi4iot <osi4iot@gmail.com>
2022-06-09 20:14:25 +0200

    Fixing efs storage problems 9

--- utils/osi4iot_cli/src/config_tools/stackFileGenerator.js
+++ utils/osi4iot_cli/src/config_tools/stackFileGenerator.js
@@ -926,12 +926,11 @@ export default function (osi4iotState) {
 
 			osi4iotStackObj.volumes['pgdata'] = {
 				driver: 'local',
-				driver_opts: {
-					type: 'nfs',
-					o: `addr=${efs_dns},nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport`,
-					// device: `${efs_dns}:/pgdata`
-					device: `:/pgdata`
-				}
+				// driver_opts: {
+				// 	type: 'nfs',
+				// 	o: `addr=${efs_dns},nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport`,
+				// 	device: `${efs_dns}:/pgdata`
+				// }
 			}
 
 			osi4iotStackObj.volumes['pgadmin4_data'] = {
