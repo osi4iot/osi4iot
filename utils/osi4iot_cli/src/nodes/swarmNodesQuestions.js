e58e8c66688e5c188234d297547794df98d1be8d
osi4iot <osi4iot@gmail.com>
2022-06-11 13:07:07 +0200

    Trying to solve connection problems 8

--- utils/osi4iot_cli/src/nodes/swarmNodesQuestions.js
+++ utils/osi4iot_cli/src/nodes/swarmNodesQuestions.js
@@ -87,6 +87,7 @@ const swarmNodeQuestions = async (nodesData, defaultUserName, numSwarmNodes, dep
 			},
 		])
 		.then(async (newNode) => {
+			console.log("Paso por aqui 1.05")
 			const userName = newNode.nodeUserName;
 			const nodeIP = newNode.nodeIP;
 			if (numSwarmNodes === 1) {
@@ -95,19 +96,22 @@ const swarmNodeQuestions = async (nodesData, defaultUserName, numSwarmNodes, dep
 
 			let status = "OK";
 			if (!isLocahostNode(nodeIP)) {
+				console.log("Paso por aqui 1.1")
 				if (inode === 1 && deploymentLocation === "AWS cluster deployment") {
+					console.log("Paso por aqui 1.2")
 					console.log(clc.redBright("Error: The first node in AWS cluster deployment must be the localhost."));
 					status = "Failed"
 				} else {
+					console.log("Paso por aqui 1.3")
 					if (deploymentLocation !== "AWS cluster deployment") {
 						status = await sshCopyId(userName, nodeIP);
 					}
 					if (status === "Failed") {
 						console.log(clc.redBright("Error: Connection with the indicated node cannot be established"));
 					} else {
-						console.log("Paso por aqui 1.1")
+						console.log("Paso por aqui 1.4")
 						const nodeArch = execSync(`ssh ${userName}@${nodeIP} 'uname -m'`).toString().trim();
-						console.log("Paso por aqui 1.2")
+						console.log("Paso por aqui 1.5")
 						if (!(nodeArch === "x86_64" || nodeArch === "aarch64")) {
 							console.log(clc.redBright("Error: Only are supported amd64 and arm64 architectures"));
 							status = "Failed";
