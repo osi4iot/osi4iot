ec0518707d2887f9d35fcece355d788b84891deb
osi4iot <osi4iot@gmail.com>
2022-07-27 12:26:24 +0200

    Allowing branches in installer

--- utils/osi4iot_cli/dist/linux_arm64/osi4iot_installer_linux_arm64.sh
+++ utils/osi4iot_cli/dist/linux_arm64/osi4iot_installer_linux_arm64.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-echo "Downloading osi4iot_cli..."
-curl -o osi4iot https://raw.githubusercontent.com/osi4iot/osi4iot/master/utils/osi4iot_cli/dist/linux_arm64/osi4iot &> /dev/null
-echo "osi4iot_cli downloaded."
-sudo mv osi4iot /usr/local/bin/osi4iot
-sudo chmod +x /usr/local/bin/osi4iot
-echo "osi4iot_cli installed."
+branch=$1
+if [ -z "$var" ]
+then
+    branch=master
+fi
+
+url=https://raw.githubusercontent.com/osi4iot/osi4iot/$branch/utils/osi4iot_cli/dist/linux_arm64/osi4iot
+
+if curl --head --fail --silent "$url" >/dev/null; then
+    echo "Downloading osi4iot_cli..."
+    curl -o osi4iot "$url" &> /dev/null
+    echo "osi4iot_cli downloaded."
+    sudo mv osi4iot /usr/local/bin/osi4iot
+    sudo chmod +x /usr/local/bin/osi4iot
+    echo "osi4iot_cli installed."
+fi
