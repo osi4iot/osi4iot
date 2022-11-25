e9632302c24872c58d343d772f09c184b0da0a5e
osi4iot <osi4iot@gmail.com>
2022-11-25 19:54:59 +0100

    Frontend config update 4

--- utils/osi4iot_cli/src/config_tools/checkFrontendConfigFile.js
+++ utils/osi4iot_cli/src/config_tools/checkFrontendConfigFile.js
@@ -6,7 +6,7 @@ export default function (osi4iotState) {
     if (fs.existsSync(frontend_conf_file)) {
         let frontendConfFileNeedToBeUpdated = false;
         const frontendConfFileText = fs.readFileSync(frontend_conf_file, 'UTF-8');
-        const frontendConfFileArray = frontendConfFileText.split("/n");
+        const frontendConfFileArray = frontendConfFileText.split("\n");
 
         const deploymentModeInConfigFile = frontendConfFileArray.filter(row => row.slice(0, 15) === "DEPLOYMENT_MODE")[0].slice(15);
         if (deploymentModeInConfigFile !== osi4iotState.platformInfo.DEPLOYMENT_MODE) {
