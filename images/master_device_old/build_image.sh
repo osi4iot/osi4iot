611bc00e158bec94fea958a9161fbdcc63edbde8
osi4iot <osi4iot@gmail.com>
2023-01-26 09:50:33 +0100

    Removing old images

--- images/master_device_old/build_image.sh
+++
@@ -1,2 +0,0 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/osi4iot/master_device:1.1.0 --push .
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/osi4iot/master_device:latest --push .
