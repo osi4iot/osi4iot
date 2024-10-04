465696c0de1a3fb48e49f70274bc66a2dfdff96b
DanielDiCapua <dicapua67@gmail.com>
2024-10-04 11:30:26 +0200

    Mofing execShellComand.js file

--- utils/osi4iot_cli/src/generic_tools/execShellCommand.js
+++ utils/osi4iot_cli/src/generic_tools/execShellCommand.js
@@ -1,22 +1,3 @@
-// import { spawn } from 'child_process';
-
-// export default function (cmd, options = {}) {
-// 	const commandParameters = cmd.split(" ").filter(param => param !== "");
-// 	return new Promise(function (resolve, reject) {
-// 		const child = spawn(commandParameters[0], [...commandParameters.slice(1)], options);
-// 		child.stdout.on('data', (chunk) => {
-// 			process.stdout.write(chunk.toString())
-// 		});
-// 		// since these are streams, you can pipe them elsewhere
-// 		child.stderr.on('data', msg => {
-// 			reject(msg.toString())
-// 		});
-// 		child.on('close', (code) => {
-// 			resolve(code);
-// 		});
-// 	});
-// }
-
 import { spawn } from 'child_process';
 
 export default function execShellCommand(cmd, options = {}) {
