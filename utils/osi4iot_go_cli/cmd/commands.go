9f10b1a9e170e814d4e3e17dc0c7845740d317a1
DanielDiCapua <dicapua67@gmail.com>
2026-06-02 17:11:02 +0200

    Vector with rex-ray plugin test 2 in AWS

--- utils/osi4iot_go_cli/cmd/commands.go
+++ utils/osi4iot_go_cli/cmd/commands.go
@@ -17,7 +17,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.1.1"
+const version = "0.1.2"
 
 var SwarmActions = []string{"create", "init", "run", "stop", "delete", "service", "certs"}
 
