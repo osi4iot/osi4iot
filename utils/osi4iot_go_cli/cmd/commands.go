50a5c76d56b23e574bbe43b07431bf68120233b3
DanielDiCapua <dicapua67@gmail.com>
2026-06-14 09:38:21 +0200

    Scale down services for rexray-ebs driver 2

--- utils/osi4iot_go_cli/cmd/commands.go
+++ utils/osi4iot_go_cli/cmd/commands.go
@@ -18,7 +18,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.1.17"
+const version = "0.1.18"
 
 var SwarmActions = []string{"create", "init", "run", "stop", "delete", "service", "certs"}
 
